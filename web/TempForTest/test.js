8554147577320ded4f983c35997d11d28357bc25
Kish29 <875691208@qq.com>
2020-07-10 01:50:55 +0800

    还是把形状换成了圆形

--- web/TempForTest/test.js
+++ web/TempForTest/test.js
@@ -14,7 +14,7 @@ class BasicShape {
         context.fillStyle = this.color;
         context.beginPath();
         //x, y, radius, start_angle, end_angle, anti-clockwise
-        context.arc(0, 0, this.radius, this.deg, this.deg + Math.PI, true);
+        context.arc(0, 0, this.radius, 0, Math.PI * 2, true);
         context.closePath();
         context.fill();
         context.restore();
@@ -22,9 +22,9 @@ class BasicShape {
 }
 
 function moveSakura(snow, index) {
-    snow.y += snow.radius / 3; // 下降位移量
+    snow.y += 1.5; // 下降位移量
     snow.x += -1.3 * Math.random(); // 左飘位移量
-    snow.deg += Math.PI / 50;
+    //snow.deg += Math.PI / 250;
     if (snow.y > canvas.height || snow.x < 0) { // 飘出画布的樱花花消去
         snow = null;
         sakuraList.splice(index, 1);
@@ -36,7 +36,7 @@ function moveSakura(snow, index) {
 function createSakura() {
     let now = new Date();
     if (now - lastSnowCreatedTime > sakuraList.length - now.getMinutes() && sakuraList.length < 1200) {
-        const radius = Math.random() * 6 + 2;
+        const radius = Math.random() * 2 + 2;
         let sakura = new BasicShape(radius);
         // 每一分钟的前5秒在屏幕大范围内生成樱花
         let timeGap = now.getSeconds() - firstSnowCreatedTime.getSeconds();
@@ -45,7 +45,7 @@ function createSakura() {
         else // 5秒之后随机在顶部或者屏幕中生成樱花
             sakura.y = Math.random() > 0.5 ? 0 : Math.random() * canvas.height + 1;
         sakura.x = Math.random() * canvas.width + 1;
-        let alpha = Math.random() + 0.5;
+        let alpha = Math.random() + 0.3;
         sakura.color = 'rgba(255,192,203,' + alpha + ')';
         sakuraList.push(sakura);
         lastSnowCreatedTime = now;
@@ -72,6 +72,6 @@ let sakuraList = [];
 let canvas = document.getElementById('myCanvas'),
     context = canvas.getContext('2d');
 let backImage = new Image();
-backImage.src = '/BKforum/web/Images/Background/ame.jpg';
+backImage.src = "/BKforum/web/Images/Background/yaesakura.jpg"; 
 
 drawFrame();
